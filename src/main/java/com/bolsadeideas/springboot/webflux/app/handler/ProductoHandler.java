3f3eae5b530daf4acacbe0e0b8d1d0e4b6bfe8b6
miguel <miguel.blas_03@hotmail.com>
2021-04-13 13:14:05 -0500

    router function

--- src/main/java/com/bolsadeideas/springboot/webflux/app/handler/ProductoHandler.java
+++ src/main/java/com/bolsadeideas/springboot/webflux/app/handler/ProductoHandler.java
@@ -1,5 +1,8 @@
 package com.bolsadeideas.springboot.webflux.app.handler;
 
+import java.net.URI;
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
@@ -19,5 +22,45 @@ public class ProductoHandler {
 
 	public Mono<ServerResponse> listar(ServerRequest request) {
 		return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON).body(service.findAll(), Producto.class);
+	}
+
+	public Mono<ServerResponse> ver(ServerRequest request) {
+		String id = request.pathVariable("id");
+		return service.findById(id)
+				.flatMap(p -> ServerResponse.ok().contentType(MediaType.APPLICATION_JSON).bodyValue(p))
+				.switchIfEmpty(ServerResponse.notFound().build());
+	}
+
+	public Mono<ServerResponse> crear(ServerRequest request) {
+		Mono<Producto> producto = request.bodyToMono(Producto.class);
+		return producto.flatMap(p -> {
+			if (p.getFecha() == null) {
+				p.setFecha(new Date());
+			}
+			return service.save(p);
+		}).flatMap(p -> ServerResponse.created(URI.create("/api/v2/productos/".concat(p.getId())))
+				.contentType(MediaType.APPLICATION_JSON).bodyValue(p));
+	}
+
+	public Mono<ServerResponse> editar(ServerRequest request) {
+		Mono<Producto> producto = request.bodyToMono(Producto.class);
+		String id = request.pathVariable("id");
+		Mono<Producto> productoDb = service.findById(id);
+
+		return productoDb.zipWith(producto, (db, req) -> {
+			db.setNombre(req.getNombre());
+			db.setPrecio(req.getPrecio());
+			db.setCategoria(req.getCategoria());
+			return db;
+		}).flatMap(p -> ServerResponse.created(URI.create("/api/v2/productos/".concat(p.getId())))
+				.contentType(MediaType.APPLICATION_JSON).body(service.save(p), Producto.class))
+				.switchIfEmpty(ServerResponse.notFound().build());
+	}
+
+	public Mono<ServerResponse> eliminar(ServerRequest request) {
+		String id = request.pathVariable("id");
+		Mono<Producto> productoDb = service.findById(id);
+		return productoDb.flatMap(p -> service.delete(p).then(ServerResponse.noContent().build()))
+				.switchIfEmpty(ServerResponse.notFound().build());
 	}
 }
