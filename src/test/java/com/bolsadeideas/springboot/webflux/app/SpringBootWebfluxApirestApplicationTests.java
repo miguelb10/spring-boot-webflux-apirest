e11769d3611bc82c63869ea5633d0df48e229800
miguel <miguel.blas_03@hotmail.com>
2021-04-14 13:15:06 -0500

    test

--- src/test/java/com/bolsadeideas/springboot/webflux/app/SpringBootWebfluxApirestApplicationTests.java
+++ src/test/java/com/bolsadeideas/springboot/webflux/app/SpringBootWebfluxApirestApplicationTests.java
@@ -1,13 +1,66 @@
 package com.bolsadeideas.springboot.webflux.app;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import java.util.Collections;
+import java.util.List;
 
-@SpringBootTest
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import com.bolsadeideas.springboot.webflux.app.models.documents.Producto;
+import com.bolsadeideas.springboot.webflux.app.models.services.ProductoService;
+
+import reactor.core.publisher.Mono;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SpringBootWebfluxApirestApplicationTests {
 
+	@Autowired
+	private WebTestClient client;
+	
+	@Autowired
+	private ProductoService service;
+	
 	@Test
-	void contextLoads() {
+	public void listarTest() {
+		client.get()
+		.uri("/api/v2/productos")
+		.accept(MediaType.APPLICATION_JSON)
+		.exchange()
+		.expectStatus().isOk()
+		.expectHeader().contentType(MediaType.APPLICATION_JSON)
+		.expectBodyList(Producto.class)
+		.consumeWith(response -> {
+			List<Producto> productos = response.getResponseBody();
+			productos.forEach(p -> {
+				System.out.println(p.getNombre());
+			});
+			Assertions.assertThat(productos.size()>0).isTrue();
+		});
+		//.hasSize(9);
+	}
+	
+	@Test
+	public void verTest() {
+		Producto producto = service.findByNombre("TV Panasonic").block();
+		client.get()
+		.uri("/api/v2/productos/{id}", Collections.singletonMap("id", producto.getId()))
+		.accept(MediaType.APPLICATION_JSON)
+		.exchange()
+		.expectStatus().isOk()
+		.expectHeader().contentType(MediaType.APPLICATION_JSON)
+		.expectBody(Producto.class)
+		.consumeWith(response -> {
+			Producto p = response.getResponseBody();
+			Assertions.assertThat(p.getId()).isNotEmpty();
+			Assertions.assertThat(p.getNombre()).isEqualTo("TV Panasonic");
+		});
+		/*.expectBody()
+		.jsonPath("$.id").isNotEmpty()
+		.jsonPath("$.nombre").isEqualTo("TV Panasonic");*/
 	}
 
 }
