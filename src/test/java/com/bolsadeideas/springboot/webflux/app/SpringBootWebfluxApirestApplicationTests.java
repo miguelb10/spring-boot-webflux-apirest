6063e81bac4c52115d3f19aed031c7bc82e13ea5
miguel <miguel.blas_03@hotmail.com>
2021-04-14 19:31:15 -0500

    finish test

--- src/test/java/com/bolsadeideas/springboot/webflux/app/SpringBootWebfluxApirestApplicationTests.java
+++ src/test/java/com/bolsadeideas/springboot/webflux/app/SpringBootWebfluxApirestApplicationTests.java
@@ -1,21 +1,28 @@
 package com.bolsadeideas.springboot.webflux.app;
 
 import java.util.Collections;
+import java.util.LinkedHashMap;
 import java.util.List;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
+import com.bolsadeideas.springboot.webflux.app.models.documents.Categoria;
 import com.bolsadeideas.springboot.webflux.app.models.documents.Producto;
 import com.bolsadeideas.springboot.webflux.app.models.services.ProductoService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import reactor.core.publisher.Mono;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWebTestClient
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 class SpringBootWebfluxApirestApplicationTests {
 
 	@Autowired
@@ -24,10 +31,13 @@ class SpringBootWebfluxApirestApplicationTests {
 	@Autowired
 	private ProductoService service;
 	
+	@Value("${config.base.endpoint}")
+	private String url;
+	
 	@Test
 	public void listarTest() {
 		client.get()
-		.uri("/api/v2/productos")
+		.uri(url)
 		.accept(MediaType.APPLICATION_JSON)
 		.exchange()
 		.expectStatus().isOk()
@@ -47,7 +57,7 @@ class SpringBootWebfluxApirestApplicationTests {
 	public void verTest() {
 		Producto producto = service.findByNombre("TV Panasonic").block();
 		client.get()
-		.uri("/api/v2/productos/{id}", Collections.singletonMap("id", producto.getId()))
+		.uri(url + "/{id}", Collections.singletonMap("id", producto.getId()))
 		.accept(MediaType.APPLICATION_JSON)
 		.exchange()
 		.expectStatus().isOk()
@@ -63,4 +73,79 @@ class SpringBootWebfluxApirestApplicationTests {
 		.jsonPath("$.nombre").isEqualTo("TV Panasonic");*/
 	}
 
+	@Test
+	public void crearTest() {
+		Categoria categoria = service.findCategoriaByNombre("Muebles").block();
+		Producto producto = new Producto("Mesa comedor", 1000.00, categoria);
+		client.post().uri(url)
+		.contentType(MediaType.APPLICATION_JSON)
+		.accept(MediaType.APPLICATION_JSON)
+		.body(Mono.just(producto), Producto.class)
+		.exchange()
+		.expectStatus().isCreated()
+		.expectHeader().contentType(MediaType.APPLICATION_JSON)
+		.expectBody()
+		.jsonPath("$.producto.id").isNotEmpty()
+		.jsonPath("$.producto.nombre").isEqualTo("Mesa comedor")
+		.jsonPath("$.producto.categoria.nombre").isEqualTo("Muebles");
+	}
+	
+	@Test
+	public void crear2Test() {
+		Categoria categoria = service.findCategoriaByNombre("Muebles").block();
+		Producto producto = new Producto("Mesa comedor", 1000.00, categoria);
+		client.post().uri(url)
+		.contentType(MediaType.APPLICATION_JSON)
+		.accept(MediaType.APPLICATION_JSON)
+		.body(Mono.just(producto), Producto.class)
+		.exchange()
+		.expectStatus().isCreated()
+		.expectHeader().contentType(MediaType.APPLICATION_JSON)
+		.expectBody(new ParameterizedTypeReference<LinkedHashMap<String, Object>>() {})
+		.consumeWith(response -> {
+			Object o = response.getResponseBody().get("producto");
+			Producto p = new ObjectMapper().convertValue(o, Producto.class);
+			Assertions.assertThat(p.getId()).isNotEmpty();
+			Assertions.assertThat(p.getNombre()).isEqualTo("Mesa comedor");
+			Assertions.assertThat(p.getCategoria().getNombre()).isEqualTo("Muebles");
+		});
+	}
+	
+	@Test
+	public void editarTest() {
+		Producto producto = service.findByNombre("Sony Camara").block();
+		Categoria categoria = service.findCategoriaByNombre("Electrónico").block();
+		
+		Producto productoEditado = new Producto("Panasonic Camara", 700.00, categoria);
+		client.put().uri(url + "/{id}", Collections.singletonMap("id", producto.getId()))
+		.contentType(MediaType.APPLICATION_JSON)
+		.accept(MediaType.APPLICATION_JSON)
+		.body(Mono.just(productoEditado), Producto.class)
+		.exchange()
+		.expectStatus().isCreated()
+		.expectHeader().contentType(MediaType.APPLICATION_JSON)
+		.expectBody()
+		.jsonPath("$.id").isNotEmpty()
+		.jsonPath("$.nombre").isEqualTo("Panasonic Camara")
+		.jsonPath("$.categoria.nombre").isEqualTo("Electrónico");
+	}
+	
+	@Test
+	public void eliminarTest() {
+		Producto producto = service.findByNombre("Mica comoda 5").block();
+		client.delete()
+		.uri(url + "/{id}", Collections.singletonMap("id", producto.getId()))
+		.exchange()
+		.expectStatus().isNoContent()
+		.expectBody()
+		.isEmpty();
+		
+		client.get()
+		.uri(url + "/{id}", Collections.singletonMap("id", producto.getId()))
+		.exchange()
+		.expectStatus().isNotFound()
+		.expectBody()
+		.isEmpty();
+	}
+	
 }
